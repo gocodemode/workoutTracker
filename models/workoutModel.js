637421eb12d92f4b12f4b93494a2a72507d9435a
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-27 15:39:49 -0400

    deleted files to keep entire workout options on one file.

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -2,14 +2,65 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: { virtuals: true }};
+
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
     },
-    exercises: []
+    exercises: [
+        {
+            type: {
+                type: String,
+                enum: ["resistance", "cardio"],
+                required: "exercise type is required"
+            },
+            
+            name: {
+                type: String,
+                trim: true,
+                required: "exercise name is required"
+        
+            },
+        
+            duration: {
+                type: Number,
+                allowNull:false
+            },
+        
+            distance: {
+                type: Number,
+                allowNull: true
+            },
+            weight: {
+                type: Number,
+                allowNull: true
+            },
+        
+            reps: {
+                type: Number,
+                allowNull: true
+            },
+        
+            sets: {
+                type: Number,
+                allowNull: true
+            }
+        
+        
+        }
+    ]
+}, opts);
+
+WorkoutSchema.virtual('totalDuration').get(function() {
+    let totalDuration = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+       totalDuration += this.exercises[i].duration;
+    }
+        return totalDuration;
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema)
-;
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
 module.exports = Workout;
