dd2353b0fe379d0d663a17936ba20ab9b2c25b16
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-27 15:57:16 -0400

    cleaned up apiRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,48 @@
-// var db = require("../models");
-
-// module.exports = function(app) {
-
 var path = require("path");
+
 const db = require("../models");
+
 module.exports = function(app) {
 
-app.get("/api/config", (req, res) => {
-    db.Workout.find({}).then((foundWorkouts) => {
-        res.json({
-            success: true,
-            data: foundWorkouts,
-        });
-    });
-});
-//Find all workouts
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then((foundWorkouts) => {
-        res.json(foundWorkouts);
-    });
-});
-app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).limit(7).then((foundWorkouts) => {
-        res.json(foundWorkouts);
-    });
-});
-//Update workout by pushing exercise into array.
-app.put("/api/workouts/:id", (req, res) => {
-    const id = req.params.id;
-    db.Workout.findByIdAndUpdate(id, {$push: {"exercises": req.body}},{"new": true}).then((updatedWorkout) => {
-        res.json(updatedWorkout);
-    });
-});
-//Create a new workout
-app.post("/api/workouts", (req, res) => {
-    
-    Workout.create(req.body).then(createdWorkout => {
-        res.json(createdWorkout)
-    }).catch(err => {
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "Unable to create new workout."
-        });
-    });
-});
+  app.get("/api/config", (req, res) => {
+      db.Workout.find({}).then((foundWorkouts) => {
+          res.json({
+              success: true,
+              data: foundWorkouts,
+          });
+      });
+  });
+  //Find all workouts
+  app.get("/api/workouts", (req, res) => {
+      db.Workout.find({}).then((foundWorkouts) => {
+          res.json(foundWorkouts);
+      });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+      db.Workout.find({}).limit(7).then((foundWorkouts) => {
+          res.json(foundWorkouts);
+      });
+  });
+  //Update workout by pushing exercise into array.
+  app.put("/api/workouts/:id", (req, res) => {
+      const id = req.params.id;
+      db.Workout.findByIdAndUpdate(id, {$push: {"exercises": req.body}},{"new": true}).then((updatedWorkout) => {
+          res.json(updatedWorkout);
+      });
+  });
+  //Create a new workout
+  app.post("/api/workouts", (req, res) => {
+      
+      Workout.create(req.body).then(createdWorkout => {
+          res.json(createdWorkout)
+      }).catch(err => {
+          console.log(err);
+          res.json({
+              error: true,
+              data: null,
+              message: "Unable to create new workout."
+          });
+      });
+  });
+};
