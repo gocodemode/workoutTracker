050f742735210d34ed7032ead7de83ac7d4af3e5
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-26 21:20:54 -0400

    adjusted server.js file and cleaned up the code in cardio.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     
 app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
+});
 
 app.get("/exercise", function(req,res)  {
     res.sendFile(path.join(__dirname, "../public/exercise.html"))
--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
-const app = express();
-
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect (
+    process.env.MONGODB_URI || "mongodb://mongodb://localhost/workoutTracker",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+); 
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+});
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true
+    })
+});
+
 require("./routes/htmlRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`Server is running on http://localhost:${PORT}!`);
 });
